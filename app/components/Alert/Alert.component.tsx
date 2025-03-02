cb007ee5e2b3403daa13f2b5f7c9b3c66a645a8d
Saeid Barzegar <saeidbarzegar@gmail.com>
2025-03-02 15:34:29 +0330

    Alert component added

---
+++ app/components/Alert/Alert.component.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { IoCloseOutline } from "react-icons/io5";
+import { AlertPropTypes } from '@/app/types/components.type';
+import { TIMER_VALUE, BACKGROUND_COLOR_MAPPER, TEXT_COLOR_MAPPER } from "./constants"
+import styles from "./style.module.scss";
+
+const Alert: React.FC<AlertPropTypes> = ({
+  mode = "info",
+  message = "",
+  onClose = () => { }
+}) => {
+
+  useEffect(() => {
+    if (!message) return;
+    const alertTimeout = setTimeout(() => onClose(), TIMER_VALUE);
+    return () => clearTimeout(alertTimeout);
+  }, [message]);
+
+  return (
+    <div className={`${styles.container} ${BACKGROUND_COLOR_MAPPER[mode]}`}>
+      <button
+        onClick={onClose}
+        className={styles.closeButton}
+      >
+        <IoCloseOutline
+          className={`${styles.closeIcon} ${TEXT_COLOR_MAPPER[mode]}`}
+        />
+      </button>
+      <span className={`${styles.message} ${TEXT_COLOR_MAPPER[mode]}`}>{message}</span>
+    </div>
+  )
+}
+
+export default Alert
---
+++ app/components/Alert/constants.ts
@@ -0,0 +1,17 @@
+import styles from "./style.module.scss";
+
+export const TIMER_VALUE = 5000;
+
+export const BACKGROUND_COLOR_MAPPER = {
+  info: styles.infoBackground,
+  success: styles.successBackground,
+  warning: styles.warningBackground,
+  danger: styles.dangerBackground,
+};
+
+export const TEXT_COLOR_MAPPER = {
+  info: styles.infoColor,
+  success: styles.successColor,
+  warning: styles.warningColor,
+  danger: styles.dangerColor,
+};
